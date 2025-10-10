09c04bf27aa2db6a245324189bf68625239329f8
Patrick Stegaribe <patrickstegaribe@gmail.com>
2025-10-10 04:38:12 +0000

    Try fixing this error: `Runtime Error: Error: useFirebase must be used w

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,15 +2,12 @@
 
 import React from 'react';
 import Sidebar from '@/components/layout/sidebar';
-import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <FirebaseClientProvider>
-      <div className="flex h-full">
-        <Sidebar />
-        <main className="flex-1 h-full overflow-hidden">{children}</main>
-      </div>
-    </FirebaseClientProvider>
+    <div className="flex h-full">
+      <Sidebar />
+      <main className="flex-1 h-full overflow-hidden">{children}</main>
+    </div>
   );
 }
