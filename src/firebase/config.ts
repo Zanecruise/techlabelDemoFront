33d8bdf8767f27561a593be46ca6f1c7f2e06392
Patrick Stegaribe <patrickstegaribe@gmail.com>
2025-10-10 03:29:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1199601499-5a4f7",
+  "appId": "1:981765484547:web:ea45f8783700b9566d1e36",
+  "apiKey": "AIzaSyCs7DpBa_oJKMcj8vJQd12TD58A0APzpj8",
+  "authDomain": "studio-1199601499-5a4f7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "981765484547"
+};
