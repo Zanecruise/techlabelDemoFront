a1b1897f3d069c1624bd75bab6d07f42fca2ac86
Patrick Stegaribe <patrickstegaribe@gmail.com>
2025-11-06 17:02:21 +0000

    arrume os dois deseing que voce tirou, no total sao 8 templates

--- src/lib/designs.ts
+++ src/lib/designs.ts
@@ -6,4 +6,6 @@ export const designs = [
     { id: 'template-4', name: 'Template Nutricional', imageUrl: '/images/template4.jpg', fields: ['Nome do Produto', 'Valor', 'Valor por Unidade'] },
     { id: 'template-5', name: 'Template Colunas', imageUrl: '/images/template5.jpg', fields: ['Nome do Produto', 'Valor', 'Info Coluna 1', 'Info Coluna 2'] },
     { id: 'template-6', name: 'Template Minimalista', imageUrl: '/images/template6.jpg', fields: ['Nome do Produto', 'Valor'] },
+    { id: 'template-7', name: 'Template com Imagem', imageUrl: '/images/template7.jpg', fields: ['Nome do Produto', 'Valor', 'URL da Imagem'] },
+    { id: 'template-8', name: 'Template de Alerta', imageUrl: '/images/template8.jpg', fields: ['Nome do Produto', 'Valor', 'Mensagem de Alerta'] },
 ];
