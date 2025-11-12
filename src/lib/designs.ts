a59cab7773348c14354e49f85a86d5546c67f653
Patrick Stegaribe <patrickstegaribe@gmail.com>
2025-11-12 23:42:49 +0000

    minhas imagens nao estao sendo atualizadas nos templates, configurei esl

--- src/lib/designs.ts
+++ src/lib/designs.ts
@@ -1,11 +1,12 @@
-
 export const designs = [
   { id: 'template-1', name: 'Template 1', imageUrl: '/images/template1.jpg', fields: ['nome', 'precoKg', 'sku', 'data', 'precoStr'] },
   { id: 'template-2', name: 'Template 2', imageUrl: '/images/template2.jpg', fields: ['nome', 'precoKg', 'sku', 'data', 'precoStr'] },
   { id: 'template-3', name: 'Template 3', imageUrl: '/images/template3.jpg', fields: ['marca', 'produto', 'preco', 'texto1', 'texto2'] },
-  { id: 'template-4', name: 'Template 4', imageUrl: '/images/template4.jpg', fields: ['marca', 'produto', 'preco', 'precoDesconto', 'texto1', 'texto2'] },
-  { id: 'template-7', name: 'Template 7', imageUrl: '/images/template5.jpg', fields: ['texto1'] },
-  { id: 'template-8', name: 'Template 8', imageUrl: '/images/template6.jpg', fields: ['texto1'] },
-  { id: 'template-9', name: 'Template 9', imageUrl: '/images/template7.jpg', fields: ['texto1', 'texto2'] },
-  { id: 'template-10', name: 'Template 10', imageUrl: '/images/template8.jpg', fields: ['texto1', 'texto2'] }
+  { id: 'template-4', name: 'Template 4', imageUrl: '/images/template4.jpg', fields: ['marca', 'produto', 'preco', 'texto1', 'texto2'] },
+  { id: 'template-5', name: 'Template 5', imageUrl: '/images/template5.jpg', fields: ['marca', 'produto', 'preco', 'precoDesconto', 'texto1', 'texto2'] },
+  { id: 'template-6', name: 'Template 6', imageUrl: '/images/template6.jpg', fields: ['marca', 'produto', 'preco', 'precoDesconto', 'texto1', 'texto2'] },
+  { id: 'template-7', name: 'Template 7', imageUrl: '/images/template7.jpg', fields: ['texto1'] },
+  { id: 'template-8', name: 'Template 8', imageUrl: '/images/template8.jpg', fields: ['texto1'] },
+  { id: 'template-9', name: 'Template 9', imageUrl: '/images/template9.jpg', fields: ['texto1', 'texto2'] },
+  { id: 'template-10', name: 'Template 10', imageUrl: '/images/template10.jpg', fields: ['texto1', 'texto2'] }
 ];
