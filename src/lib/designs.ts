a74a9bf189f2d90a196ba08f6e5be0b7cb8d8372
Patrick Stegaribe <patrickstegaribe@gmail.com>
2025-11-12 23:58:57 +0000

    v1

--- src/lib/designs.ts
+++ src/lib/designs.ts
@@ -6,5 +6,7 @@ export const designs = [
   { id: 'template-5', name: 'Template 5', imageUrl: '/images/template05.jpg', fields: ['marca', 'produto', 'preco', 'precoDesconto', 'texto1', 'texto2'] },
   { id: 'template-6', name: 'Template 6', imageUrl: '/images/template06.jpg', fields: ['marca', 'produto', 'preco', 'precoDesconto', 'texto1', 'texto2'] },
   { id: 'template-7', name: 'Template 7', imageUrl: '/images/template07.jpg', fields: ['texto1'] },
-  { id: 'template-8', name: 'Template 8', imageUrl: '/images/template08.jpg', fields: ['texto1', 'texto2'] }
+  { id: 'template-8', name: 'Template 8', imageUrl: '/images/template08.jpg', fields: ['texto1'] },
+  { id: 'template-9', name: 'Template 9', imageUrl: '/images/template09.jpg', fields: ['texto1', 'texto2'] },
+  { id: 'template-10', name: 'Template 10', imageUrl: '/images/template10.jpg', fields: ['texto1', 'texto2'] }
 ];
